8a6a44d172cd993bb950885f988a67d878a9b7bf
ehesp <elliot.hesp@gmail.com>
2021-09-20 16:55:51 +0100

    feat: update hook

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,57 +1,43 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import {
   QueryKey,
   useQuery,
   useQueryClient,
   UseQueryOptions,
 } from "react-query";
-import { Auth, onIdTokenChanged, User } from "firebase/auth";
-import {
-  Completer,
-  usePrevious,
-  getClientKey,
-} from "@react-query-firebase/utils";
+import { Auth, onIdTokenChanged, User, Unsubscribe } from "firebase/auth";
 
 export function useAuthUser(
   key: QueryKey,
   auth: Auth,
-  useQueryOptions?: UseQueryOptions<User | null, Error>
+  useQueryOptions?: Omit<UseQueryOptions<User | null, Error>, "queryFn">
 ) {
   const client = useQueryClient();
-  const completer = useRef<Completer<User | null>>(new Completer());
-  const app = auth.app.name;
-  const newKey = getClientKey(key);
-  const previousKey = usePrevious(getClientKey(key));
-
-  const keyHasChanged = newKey !== previousKey;
-
-  // If there is an auth user, add it straight away rather than waiting for the
-  // listener to subscribe.
-  if (!completer.current.complete && !!auth.currentUser) {
-    completer.current.resolve(auth.currentUser);
-  }
+  const unsubscribe = useRef<Unsubscribe>();
 
   useEffect(() => {
-    completer.current = new Completer();
-  }, [keyHasChanged]);
+    return () => {
+      unsubscribe.current?.();
+    };
+  }, []);
 
-  useEffect(() => {
-    return onIdTokenChanged(auth, (state) => {
-      if (!completer.current.complete) {
-        completer.current.resolve(auth.currentUser);
-      } else {
-        client.setQueryData<User | null>(key, state);
-      }
-    });
-  }, [app, key]);
+  return useQuery<User | null, Error>({
+    queryKey: useQueryOptions?.queryKey ?? key,
+    async queryFn() {
+      unsubscribe.current?.();
 
-  return useQuery<User | null, Error>(
-    key,
-    () => {
-      return completer.current.promise;
+      let resolved = false;
+
+      return new Promise<User | null>((resolve) => {
+        unsubscribe.current = onIdTokenChanged(auth, (user) => {
+          if (!resolved) {
+            resolved = true;
+            resolve(user);
+          } else {
+            client.setQueryData<User | null>(key, user);
+          }
+        });
+      });
     },
-    {
-      ...(useQueryOptions || {}),
-    }
-  );
+  });
 }
