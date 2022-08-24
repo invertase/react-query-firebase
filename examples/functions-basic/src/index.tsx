4ea2ce5890cad17edca0692f7849ca3bb1fa6dca
kaueDM <kaue.maia123@gmail.com>
2022-08-24 09:41:05 -0300

    chore: bump examples dependencies

--- examples/functions-basic/src/index.tsx
+++ examples/functions-basic/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import {
   useFunctionsQuery,
