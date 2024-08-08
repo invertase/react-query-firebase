575d372d2b090e39da978a39902c9203e7bf58f2
HassanBahati <mukisabahati@gmail.com>
2024-08-08 18:27:51 +0300

    refactor(auth): refactor useAuthIdToken to use @tansack/react-query

--- packages/auth/src/useAuthIdToken.ts
+++ packages/auth/src/useAuthIdToken.ts
@@ -14,31 +14,36 @@
  * limitations under the License.
  *
  */
-import { QueryKey, UseQueryOptions, UseQueryResult } from "react-query";
+import {
+  QueryKey,
+  UseQueryOptions,
+  UseQueryResult,
+} from "@tanstack/react-query";
 import { Auth, AuthError, IdTokenResult } from "firebase/auth";
 import { useSubscription } from "../../utils/src/useSubscription";
 
 export function useAuthIdToken<R = { token: IdTokenResult }>(
-  queryKey: QueryKey,
   auth: Auth,
   options: Omit<
     UseQueryOptions<{ token: IdTokenResult }, AuthError, R>,
     "queryFn"
-  > = {},
+  > & { queryKey: QueryKey }
 ): UseQueryResult<R, AuthError> {
-  const subscribeFn = (
-    callback: (data: { token: IdTokenResult } | null) => Promise<void>,
-  ) =>
-    auth.onIdTokenChanged(async (data) => {
-      const token = await data?.getIdTokenResult();
+  const { queryKey } = options;
 
+  const subscribeFn = (
+    callback: (data: { token: IdTokenResult } | null) => Promise<void>
+  ) => {
+    return auth.onIdTokenChanged(async (user) => {
+      const token = await user?.getIdTokenResult();
       return callback(token ? { token } : null);
     });
+  };
 
   return useSubscription<{ token: IdTokenResult }, AuthError, R>(
     queryKey,
-    "useAuthIdToken",
+    ["useAuthIdToken"],
     subscribeFn,
-    options,
+    options
   );
 }
