aa93ed3507fb19d82cf4ed4cd31971bcb5a68f1f
Elliot Hesp <elliot.hesp@gmail.com>
2024-09-26 13:56:05 +0100

    fix: Infer types

--- packages/react/src/firestore/useGetAggregateFromServerQuery.ts
+++ packages/react/src/firestore/useGetAggregateFromServerQuery.ts
@@ -4,7 +4,8 @@ import {
   type FirestoreError,
   getAggregateFromServer,
   type AggregateSpec,
-  type AggregateField,
+  DocumentData,
+  AggregateSpecData,
 } from "firebase/firestore";
 
 type FirestoreUseQueryOptions<TData = unknown, TError = Error> = Omit<
@@ -12,23 +13,20 @@ type FirestoreUseQueryOptions<TData = unknown, TError = Error> = Omit<
   "queryFn"
 >;
 
-// map the AggregateSpec to its result type, thus resolving the AggregateField to its actual value type
-type AggregateResult<T extends AggregateSpec> = {
-  [K in keyof T]: AggregateField<T[K]> extends AggregateField<infer R>
-    ? R
-    : never;
-};
-
-export function useGetAggregateFromServerQuery<T extends AggregateSpec>(
-  query: Query,
+export function useGetAggregateFromServerQuery<
+  T extends AggregateSpec,
+  AppModelType extends DocumentData = DocumentData,
+  DbModelType extends DocumentData = DocumentData
+>(
+  query: Query<AppModelType, DbModelType>,
   aggregateSpec: T,
-  options: FirestoreUseQueryOptions<AggregateResult<T>, FirestoreError>
+  options: FirestoreUseQueryOptions<AggregateSpecData<T>, FirestoreError>
 ) {
-  return useQuery<AggregateResult<T>, FirestoreError>({
+  return useQuery<AggregateSpecData<T>, FirestoreError>({
     ...options,
     queryFn: async () => {
       const snapshot = await getAggregateFromServer(query, aggregateSpec);
-      return snapshot.data() as AggregateResult<T>;
+      return snapshot.data();
     },
   });
 }
